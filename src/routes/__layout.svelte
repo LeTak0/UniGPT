36aa25e2c3f93a27eac0bc60a11f7c1f6c594265
LeTak <tasso@kougioumtzidis.de>
2023-11-08 11:49:00 +0100

    Moved register and login svelte files into +pages with seperated directory for new routing

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,11 +1,7 @@
-<script>
-  import { goto } from '$app/navigation';
-</script>
-
 <nav>
-  <a href="/" on:click|preventDefault={() => goto('/')}>Home</a>
-  <a href="/register" on:click|preventDefault={() => goto('/register')}>Register</a>
-  <a href="/login" on:click|preventDefault={() => goto('/login')}>Login</a>
+  <a href="/">Home</a>
+  <a href="/register">Register</a>
+  <a href="/login">Login</a>
   <!-- Add other navigation links here -->
 </nav>
 
