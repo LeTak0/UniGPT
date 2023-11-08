c5e78944b3c2243e2c3598df47e6dac75201521a
LeTak <tasso@kougioumtzidis.de>
2023-11-08 13:20:56 +0100

    registration

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  // You can add any JavaScript or import statements here
+</script>
+
+<style>
+  /* Add your CSS styles here */
+  h1 {
+    text-align: center;
+    margin-top: 2rem;
+  }
+  .home-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    /*height: 90vh; */
+  }
+  nav {
+    margin-bottom: 2rem;
+  }
+
+  .logo img {
+    width: 250px;
+    height: auto;
+    position: absolute;
+    top: 0;
+    left: 0;
+    margin: 1rem;
+  }
+
+  nav {
+    padding-left: 120px; /* Adjust this value based on the new size of your logo */
+  }
+
+</style>
+
+<a href="/" class="logo">
+  <img src="/Logo_Uni_Siegen.svg" alt="UniGPT Logo" />
+</a>
+
+<div class="home-container">
+  <nav>
+    <a href="/register">Register</a>
+    <a href="/login">Login</a>
+    <!-- Add other navigation links here -->
+  </nav>
+  <h1>Welcome to UniGPT</h1>
+  <p>Your gateway to AI-powered conversations.</p>
+  <!-- Add more content or a call to action -->
+</div>
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,33 +1,40 @@
 <script>
-  import { onMount } from 'svelte';
   import { goto } from '$app/navigation';
-
   let username = '';
   let password = '';
 
-  async function register() {
-    const response = await fetch('/register', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ username, password })
-    });
+  async function register(event) {
+    // Prevent the default form submission
+    event.preventDefault();
+    console.log('Register function called'); // Debugging line
 
-    if (response.ok) {
-      // Redirect to login page or dashboard after successful registration
-      goto('/login');
-    } else {
-      // Handle errors, e.g., show an error message
-      console.error('Registration failed');
+    try {
+      const response = await fetch('auth/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password })
+      });
+
+      const result = await response.json();
+      console.log('Response received:', result); // Debugging line
+
+      if (response.ok) {
+        // Registration was successful, redirect to the login page
+        goto('/login');
+      } else {
+        // Handle errors, show message to the user
+        console.error('Registration error:', result.error);
+        // Display error message to the user
+      }
+    } catch (error) {
+      console.error('Network or other error:', error);
+      // Handle network errors or other exceptions
     }
   }
 </script>
 
-<form on:submit|preventDefault={register}>
-  <label for="username">Username:</label>
-  <input id="username" type="text" bind:value={username} required>
-
-  <label for="password">Password:</label>
-  <input id="password" type="password" bind:value={password} required>
-
+<form on:submit={register}>
+  <input type="text" bind:value={username} placeholder="Username" required>
+  <input type="password" bind:value={password} placeholder="Password" required>
   <button type="submit">Register</button>
 </form>
