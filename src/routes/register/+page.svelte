a5806279e7e81c6041e8d47c2571e605ba3d780a
Ben Ehrengruber <35403861+raketenben@users.noreply.github.com>
2023-11-08 14:52:18 +0100

    added csv

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,35 +1,40 @@
 <script>
-  import { goto } from '$app/navigation';
-  let username = '';
-  let password = '';
+	import { goto } from '$app/navigation';
+	let username = '';
+	let password = '';
 
-  async function register(event) {
-    // Prevent the default form submission
-    event.preventDefault();
-    console.log('Register function called'); // Debugging line
+	async function register(event) {
+		// Prevent the default form submission
+		event.preventDefault();
+		console.log('Register function called'); // Debugging line
 
+		try {
+			const response = await fetch('/auth/register', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({ username, password })
+			});
 
+			const result = await response.json();
+			console.log('Response received:', result); // Debugging line
 
-      const result = await response.json();
-      console.log('Response received:', result); // Debugging line
-
-      if (response.ok) {
-        // Registration was successful, redirect to the login page
-        goto('/login');
-      } else {
-        // Handle errors, show message to the user
-        console.error('Registration error:', result.error);
-        // Display error message to the user
-      }
-    } catch (error) {
-      console.error('Network or other error:', error);
-      // Handle network errors or other exceptions
-    }
-  }
+			if (response.ok) {
+				// Registration was successful, redirect to the login page
+				goto('/login');
+			} else {
+				// Handle errors, show message to the user
+				console.error('Registration error:', result.error);
+				// Display error message to the user
+			}
+		} catch (error) {
+			console.error('Network or other error:', error);
+			// Handle network errors or other exceptions
+		}
+	}
 </script>
 
 <form on:submit={register}>
-  <input type="text" bind:value={username} placeholder="Username" required>
-  <input type="password" bind:value={password} placeholder="Password" required>
-  <button type="submit">Register</button>
+	<input type="text" bind:value={username} placeholder="Username" required />
+	<input type="password" bind:value={password} placeholder="Password" required />
+	<button type="submit">Register</button>
 </form>
