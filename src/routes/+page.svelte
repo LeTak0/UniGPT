4daed4142dc617339aa6d69ba6cafdf44d05de4e
Ben Ehrengruber <35403861+raketenben@users.noreply.github.com>
2023-11-08 15:36:46 +0100

    bootstrap

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,50 +1,37 @@
 <script>
-  // You can add any JavaScript or import statements here
+	// You can add any JavaScript or import statements here
 </script>
 
-<style>
-  /* Add your CSS styles here */
-  h1 {
-    text-align: center;
-    margin-top: 2rem;
-  }
-  .home-container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    /*height: 90vh; */
-  }
-  nav {
-    margin-bottom: 2rem;
-  }
-
-  .logo img {
-    width: 250px;
-    height: auto;
-    position: absolute;
-    top: 0;
-    left: 0;
-    margin: 1rem;
-  }
-
-  nav {
-    padding-left: 120px; /* Adjust this value based on the new size of your logo */
-  }
-
-</style>
-
-<a href="/" class="logo">
-  <img src="/Logo_Uni_Siegen.svg" alt="UniGPT Logo" />
-</a>
-
-<div class="home-container">
-  <nav>
-    <a href="/register">Register</a>
-    <a href="/login">Login</a>
-    <!-- Add other navigation links here -->
-  </nav>
-  <h1>Welcome to UniGPT</h1>
-  <p>Your gateway to AI-powered conversations.</p>
-  <!-- Add more content or a call to action -->
+<div class="container-lg">
+	<nav class="navbar row navbar-expand-lg">
+		<a href="/" class="navbar-brand brand">
+			<img src="/Logo_Uni_Siegen.svg" alt="UniGPT Logo" />
+		</a>
+		<div class="collapse navbar-collapse col">
+			<a class="nav-link" href="/register">Register</a>
+			<a class="nav-link" href="/login">Login</a>
+		</div>
+	</nav>
+	<div class="container-fluid">
+		<h1>Welcome to UniGPT</h1>
+		<div class="container-sm">
+			<p>Your gateway to AI-powered conversations.</p>
+		</div>
+	</div>
 </div>
+
+<style>
+	/* Add your CSS styles here */
+	h1 {
+		text-align: center;
+		margin-top: 2rem;
+	}
+
+	nav {
+		margin-bottom: 2rem;
+	}
+
+	.brand img {
+		max-height: 5rem;
+	}
+</style>
