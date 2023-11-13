5470ce1707a11ddaf841d04236f689fdf2ba4c60
LeTak <tasso@kougioumtzidis.de>
2023-11-13 16:55:51 +0100

    Fill Impressum file

---
+++ src/routes/impressum/+page.svelte
@@ -0,0 +1,46 @@
+<script>
+  // Script section for any JavaScript needed
+</script>
+
+<style>
+  /* Style section for CSS */
+
+</style>
+
+<div class="impressum">
+  <h1>Impressum</h1>
+
+  <!-- Institution Section -->
+  <div>
+    <h2 class="section-header">Institution</h2>
+    <p>Name: Universität Siegen</p>
+    <p>Address: Adolf-Reichwein-Straße 2a</p>
+    <p>Contact: LoremIpsum Add Later</p>
+  </div>
+
+  <!-- Developers Section -->
+  <div>
+    <h2 class="section-header">Developers</h2>
+    <ul>
+      <li>Ben Ehrengruber</li>
+      <li>Anastasios Kougioumtzidis</li>
+      <!-- Add more developers as needed -->
+    </ul>
+  </div>
+
+  <!-- Project Leader Section -->
+  <div>
+    <h2 class="section-header">Project Leader</h2>
+    <p>Prof. Dr. Ingo Witzke</p>
+    <p>Lorem Ipsum Contact Info </p>
+  </div>
+
+  <!-- Team Section -->
+  <div>
+    <h2 class="section-header">Team</h2>
+    <ul>
+      <li>LoremIpsum Team Member Name 1</li>
+      <li>LoremIpsum Team Member Name 2</li>
+    </ul>
+    </div>
+</div>
