c125507e9c12d769b20071b8f0c1eca2726708a0
Ben Ehrengruber <35403861+raketenben@users.noreply.github.com>
2023-11-13 17:18:51 +0100

    api basics working

---
+++ src/lib/server/config.js
@@ -0,0 +1,6 @@
+export default {
+	//api key to be used in chat completion requests
+	openAiApiKey: "",
+	//gpt-4 / gpt-3.5-turbo
+	openAiModelName: "gpt-3.5-turbo",
+}
--- src/routes/api/chat/+server.js
+++ src/routes/api/chat/+server.js
@@ -1,11 +1,42 @@
-const delay = (/** @type {number} */ ms) => new Promise(resolve => setTimeout(resolve, ms))
+
+import { error } from '@sveltejs/kit';
+
+import config from "$lib/server/config.js";
 
 export async function POST({ request }) {
 	let { message } = await request.json();
 
 	console.log("User send message: " + message);
 
-	await delay(2000)
+	let requestBody = {
+		model: config.openAiModelName,
+		messages:[
+			{role:"system",content:"You are a math tutor. Be kind to your students and help them learn math."},
+			{role:"user",content:message}
+		]
+	}
 
-	return new Response(JSON.stringify({ success: true , message:"Welcome to my world user."}));
+	let completionRequest = await fetch("https://api.openai.com/v1/chat/completions", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"Authorization": `Bearer ${config.openAiApiKey}`
+		},
+		body: JSON.stringify(requestBody)
+	}).catch(err => {
+		console.log("Error: " + err);
+		throw error(500, "Error connecting to OpenAI API");
+	});
+
+	let completionResponse = await completionRequest.json();
+
+	if("error" in completionResponse) {
+		throw error(500, "Error from OpenAI API: " + completionResponse.error.message);
+	}
+
+	let completionText = completionResponse.choices[0].message.content;
+
+	console.log("AI response: " + completionText);
+
+	return new Response(JSON.stringify({ success: true , message: completionText}));
 }
