5171461bcc929ee21fcfafa095ab2de3614ac04e
LeTak <tasso@kougioumtzidis.de>
2023-11-14 14:09:17 +0100

    Show users on Admin page, removed create users on admin page

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,30 +1,20 @@
 <script>
     import { onMount } from 'svelte';
-    let users = [];
-    let username = '';
-    let password = '';
+
+    let users = []; // List of users
 
     // Function to fetch the list of users
     async function fetchUsers() {
-        const response = await fetch('/api/users');
-        users = await response.json();
-    }
-
-    // Function to handle form submission
-    async function createUser(event) {
-        event.preventDefault();
-        const response = await fetch('/api/register', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ username, password })
-        });
-
-        if (response.ok) {
-            username = '';
-            password = '';
-            await fetchUsers(); // Refresh the user list
-        } else {
-            console.error('Failed to create user');
+        try {
+            const response = await fetch('/api/users');
+            if (response.ok) {
+                const data = await response.json();
+                users = data.users;
+            } else {
+                console.error('Failed to fetch users');
+            }
+        } catch (error) {
+            console.error('Error fetching users:', error);
         }
     }
 
@@ -33,23 +23,30 @@
 
 <h2>Admin Panel</h2>
 
-<!-- User Creation Form -->
-<form on:submit={createUser}>
-    <label>
-        Name:
-        <input type="text" bind:value={username} />
-    </label>
-    <label>
-        Password:
-        <input type="password" bind:value={password} />
-    </label>
-    <button type="submit">Create User</button>
-</form>
+<div class="users-box">
+    <h3>Registered Users</h3>
+    {#if users.length}
+        <ul>
+            {#each users as user}
+                <li>{user.username}</li>
+            {/each}
+        </ul>
+    {:else}
+        <p>No users found.</p>
+    {/if}
+</div>
 
-<!-- User List -->
-<h3>Users:</h3>
-<ul>
-    {#each users as user}
-        <li>{user[0]}</li>
-    {/each}
-</ul>
+<style>
+    .users-box {
+        border: 1px solid #ccc;
+        padding: 10px;
+        margin-top: 20px;
+    }
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+    li {
+        margin: 5px 0;
+    }
+</style>
--- src/routes/api/users/+server.js
+++ src/routes/api/users/+server.js
@@ -1,20 +1,40 @@
 import fs from 'fs';
-import { parse } from 'csv/sync';
+import { parse, stringify } from 'csv/sync';
+import { json } from '@sveltejs/kit'; // Import the json helper
 
 const usersCsvPath = 'data/users.csv'; // Ensure this path is correct
 
-export async function get(request) {
-    // Authentication and authorization checks should go here
-    // ...
+export async function GET(request) {
+    // ... authentication and authorization checks, important that only the Admin has access to this route
+    //     also maybe add admin bool to the user object and csv file ...
 
-    // Load existing users
     let users = [];
     if (fs.existsSync(usersCsvPath)) {
         const usersCsv = fs.readFileSync(usersCsvPath);
         users = parse(usersCsv, { columns: true });
     }
 
-    // Return the list of users without passwords
     const usersWithoutPasswords = users.map(({ password, ...user }) => user);
-    return { status: 200, body: { users: usersWithoutPasswords } };
+
+    // Using json helper to return a Response object
+    return json({ users: usersWithoutPasswords });
+}
+
+export async function POST(request) {
+    // ... authentication and authorization checks, important that only the Admin has access to this route
+    // also maybe add admin bool to the user object and csv file ...
+
+    let users = [];
+    if (fs.existsSync(usersCsvPath)) {
+        const usersCsv = fs.readFileSync(usersCsvPath);
+        users = parse(usersCsv, { columns: true });
+    }
+
+    // Add new user
+    users.push(await request.json());
+    const updatedCsv = stringify(users, { header: true });
+    fs.writeFileSync(usersCsvPath, updatedCsv);
+
+    // Using json helper to return a Response object
+    return json({ success: true });
 }
